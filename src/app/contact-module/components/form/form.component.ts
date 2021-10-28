0208dbab964ceeb89b1e8fbd90b5ebe872b5fa45
Dialber <dialberob@gmail.com>
2021-10-28 11:30:52 +0100

    ADD : Mensajes de posibles errores adicionados al formulario reactivo

--- src/app/contact-module/components/form/form.component.ts
+++ src/app/contact-module/components/form/form.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, QueryList, ViewChildren } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ElementForm } from '../../models/element-form';
+import { errosRegister } from '../../models/message-errors';
 
 @Component({
   selector: 'app-form',
@@ -8,13 +10,43 @@ import { ElementForm } from '../../models/element-form';
 })
 export class FormComponent implements OnInit {
 
+  
   list:ElementForm[]=[];
+  myForm :FormGroup | undefined;
+  
 
   constructor() {
     this.list=[{name:"nombre",type:"text"},{name:"correo",type:"text"},{name:"asunto",type:"text"}];
   }
 
   ngOnInit(): void {
+    this.InitForm();
   }
-
+  
+  InitForm():void{
+    this.myForm= new FormGroup({
+      name : new FormControl('',Validators.required),
+      email : new FormControl('',[Validators.email,Validators.required]),
+      subject : new FormControl(''),
+      message : new FormControl('',Validators.required)
+    });
+  }
+  public Senddata():void{
+    console.log("********");    
+  }
+  getError(controlName:string):string{
+    const control=this.myForm?.get(controlName);
+    if(control?.invalid){
+     const messageErrors=errosRegister.find(x=>(x.formControlName==controlName))
+     const validators=messageErrors!.validators.find(element=>control.errors![element.name])
+     return validators!.message;
+    }
+    return"";
+  }
+  getControl(controlName:string):boolean | undefined{
+    if(this.myForm?.get(controlName)?.invalid && this.myForm?.get(controlName)?.dirty){
+      return true;
+    }
+    return false;
+  }
 }
---
+++ src/app/contact-module/models/message-errors.ts
@@ -0,0 +1,46 @@
+export interface MessageErrors {
+    formControlName:string;
+    validators:Validator[];
+}
+
+export interface Validator {
+    name:string;
+    message:string;
+}
+export const errosRegister:MessageErrors[]=[
+    {
+        formControlName:"name",
+        validators:[
+           {
+                name:"required",
+                message:"El usuario es requerido"
+           },
+           {
+                name:"minLength",
+                message:"El usuario debe tener mínimo 4 carácteres"
+           }
+        ]
+    },
+    {
+        formControlName:"email",
+        validators:[
+            {
+                name:"required",
+                message:"El correo es requerido"
+            },
+            {
+                name:"email",
+                message:"El correo es inválido"
+            },
+        ]
+    },
+    {
+        formControlName:"message",
+        validators:[
+            {
+                name:"required",
+                message:"El mensaje es requerido"
+            }
+        ]
+    }
+]
